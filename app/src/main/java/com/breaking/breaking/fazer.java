a5b560c18846b939873b7a82a43d965c41ae40e3
ellencsousa <ellen.sousa@gmail.com>
2016-08-20 22:50:15 -0300

    tarefas para fazer

---
+++ app/src/main/java/com/breaking/breaking/fazer.java
@@ -0,0 +1,17 @@
+package com.breaking.breaking;
+
+/**
+ * Created by Ellen on 20/08/2016.
+ */
+public class fazer {
+}
+/*
+
+pdvs - 1 aba com mapa
+      - 1 aba com a lista de pdvs
+pdv da lista - 1 aba check in - foto inicio/fim e botao check in/out
+             - 1 aba preço - lista de produtos para pegar o preço
+             - 1 aba ruptura - lista de produtos para verificar ruptura
+             - 1 aba layout - lista de medidas e disposicoes dos produtos
+             - 1 aba perg livres - lista de perguntas livres
+ */
